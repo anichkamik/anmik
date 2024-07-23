f2092d61daadbeeb7b2318ad4ffec745400f007e
anichkamik <anisamnidze@gmail.com>
2024-07-23 21:49:22 +0200

    davaleba

---
+++ axali/davaleba7.js
@@ -0,0 +1,46 @@
+let x=87
+let y=65
+console.log(x+y)
+
+let student = {
+firstname :"ana",
+lastname : "mikadze",
+age :"30",
+email: "animikadze@gmail.com",
+marks : (5,8,10,7)
+}
+
+console.log (student.firstname)
+
+let m = 45
+let n = 23
+if (m > n) { 
+    console.log ( m + n);
+} else {
+    console.log(n)
+}
+
+
+let studentname = "ana";
+console.log("rise and shine",  studentname + "!")
+
+
+
+let salary = 1800
+let bonus;
+if ( salary >= 2000) {
+    bonus = 0
+} else { bonus = salary * 0.1}
+
+console.log(bonus)
+
+let a =23
+let b = a/2
+let c = a + b
+console.log (c)
+
+
+let phones = ["iphone", "samsung", "pixel","huawei", "xiaomi"]
+console.log(phones)
+
+
